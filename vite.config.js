93d678fd6dccb07cec1051d3095c394bc033bd92
lzoxun-redmi <869664233@qq.com>
2023-10-05 15:34:55 +0800

    2023-10-05 15:34提交上传

--- vite.config.js
+++ vite.config.js
@@ -26,7 +26,7 @@ export default defineConfig({
     viteCompression({
       verbose: true, // 默认即可
       disable: false, //开启压缩(不禁用)，默认即可
-      deleteOriginFile: false, //删除源文件
+      deleteOriginFile: true, //删除源文件
       threshold: 1024, //压缩前最小文件大小
       algorithm: "gzip", //压缩算法
       ext: ".gz", //文件类型
